d50fbe5d2ceba7c638d235aba05a1319567e2754
Winston <88885652+Winstonjames2@users.noreply.github.com>
2022-06-24 14:13:49 +0630

    Update draw.js

--- draw.js
+++ draw.js
@@ -6,15 +6,15 @@ document.addEventListener("DOMContentLoaded",function(){
     let lines=[]
     let draw=false;
     
-    svg.on("mousedown",function(){
+    svg.on("mousedown" || "touchstart",function(){
         draw=true;
         const coord=d3.mouse(this);
         draw_point(coord[0],coord[1],false)
     });
-    svg.on("mouseup",function(){
+    svg.on("mouseup" || "touchend",function(){
         draw=false;
     });
-    svg.on("mousemove",function(){
+    svg.on("mousemove" || "touchmove",function(){
         const coord=d3.mouse(this);
         if(draw===false){
             return
